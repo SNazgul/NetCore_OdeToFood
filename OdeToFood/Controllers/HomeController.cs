282e13d1de31f520a9411672a2658fed4b5380ba
SNazgul <s.nazgul@gmail.com>
2017-06-11 00:20:00 +0300

    5.2. Introducing View Model

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using OdeToFood.Models;
+using OdeToFood.ViewModels;
 using OdeToFood.Services;
 
 namespace OdeToFood.Controllers
@@ -7,16 +7,19 @@ namespace OdeToFood.Controllers
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData ,IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public ViewResult Index()
         {
-            //var model = new Restaurant() { Id = 1, Name = "Sabatino's"};
-            var model = _restaurantData.GetAll();
+            var model = new HomePageViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentGreeting = _greeter.GetGreeting();
 
             //return Content("Hello, from a controller");
             return View(model);
---
+++ OdeToFood/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using OdeToFood.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace OdeToFood.ViewModels
+{
+    public class HomePageViewModel
+    {
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+        public String CurrentGreeting { get; set; }
+    }
+}
